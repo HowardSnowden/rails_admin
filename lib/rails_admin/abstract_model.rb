4d8f2f5261236e974621c0e8c08569a5f996a634
Patrice Lebel <patleb@users.noreply.github.com>
2016-09-09 00:11:53 -0400

    use exact specified time when filter is of type between

--- lib/rails_admin/abstract_model.rb
+++ lib/rails_admin/abstract_model.rb
@@ -180,8 +180,10 @@ module RailsAdmin
 
       def build_statement_for_datetime_or_timestamp
         start_date, end_date = get_filtering_duration
-        start_date = start_date.to_time.try(:beginning_of_day) if start_date
-        end_date = end_date.to_time.try(:end_of_day) if end_date
+        unless @operator == "between"
+          start_date = start_date.to_time.try(:beginning_of_day) if start_date
+          end_date = end_date.to_time.try(:end_of_day) if end_date
+        end
         range_filter(start_date, end_date)
       end
 
